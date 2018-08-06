80d1e68c2de44a2508365f6d65e352644bc2d028
Filip Panic <filip.panic.1993@gmail.com>
2018-08-06 14:50:16 +0200

    Prikaz proizvoda - json file

---
+++ WirelessMedia/Controllers/JsonController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WirelessMedia.Models;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+using System.IO;
+using System.Collections;
+
+namespace WirelessMedia.Controllers
+{
+    public class JsonController : Controller
+    {
+        public string jsonFile = "~/App_Data/Proizvodi.json";
+       
+        public ActionResult Index()
+        {
+            string file = Server.MapPath(jsonFile);
+            string json = System.IO.File.ReadAllText(file);
+            List<Proizvod> lista = JsonConvert.DeserializeObject < List < Proizvod >> (json);
+            return View(lista);
+        }
+    }
+}
